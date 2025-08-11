107cfcb37ab0674cc82ebde646f6bde7f8a2c629
takshballewar <90251245+takshballewar@users.noreply.github.com>
2025-08-11 19:01:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  p1name
+//
+//  Created by TAKSH on 18/07/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+ 
