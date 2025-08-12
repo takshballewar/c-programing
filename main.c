4a910ae3770c13fcd54a076cd6793f62679dc5c8
takshballewar <90251245+takshballewar@users.noreply.github.com>
2025-08-12 11:40:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  p6 subject 2
+//
+//  Created by TAKSH on 18/07/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int monday,tusday,wenesday,thusday,friday,total;
+    monday=22;
+    tusday=29;
+    wenesday=30;
+    thusday=45;
+    thusday=45;
+    friday=23;
+    total=monday+tusday+wenesday+thusday+friday;
+    printf("\ntotal =%d",total);
+    printf("\n");
+    return 0;
+}
