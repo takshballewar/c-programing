4ee7983b9f10c2a2a1f3c19b6e2efc9449461bbf
takshballewar <90251245+takshballewar@users.noreply.github.com>
2025-08-12 11:37:20 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("\nbunglow no 4");
+    printf("\nshree ram haretage");
+    printf("\nnana chiloda");
+    printf("\nahmedabad");
+    printf("\ngujrate");
+    
+    return 0;
+}
