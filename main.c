8546b0772bc5f979d376a683fc0b762bde90f744
takshballewar <90251245+takshballewar@users.noreply.github.com>
2025-08-15 10:16:46 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  p6 subject
+//
+//  Created by TAKSH on 18/07/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int eng,maths,ss,total;
+    
+    eng=20;
+    maths=30;
+    ss=40;
+    
+    total=eng+maths+ss;
+    
+    printf("\ntotal =%d",total);
+    printf("\n%d is total",total);
+    
+    printf("\n");
+    
+    return 0;
+}
